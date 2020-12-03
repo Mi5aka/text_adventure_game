9a3041c4662268b8d111ff3b4167f2a07bcb1fe2
Mi5aka <mi5akatwi@gmail.com>
2020-12-03 22:24:04 +0100

    locations created

---
+++ location.py
@@ -0,0 +1,13 @@
+class Location(object):
+    STREET = 'Улица'
+    OFFICE = 'Офис компании'
+    # home
+    HOME = 'Твой дом'
+    ENTERANCE = 'Подъезд'
+    # transport
+    TAXI = 'Такси'
+    METRO = 'Метро'
+
+
+def get_location(state: str) -> str:
+    pass
