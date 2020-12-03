faaf7f7849ea6a62e29782334441efa70a88551b
Mi5aka <mi5akatwi@gmail.com>
2020-12-03 22:59:13 +0100

    Structure changed and Dockerfile created

--- game/scenarios/__init__.py
+++ game/scenarios/__init__.py
@@ -5,3 +5,4 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from .scenario import Scenario
 from .data import DESCRIPTION, GAME_ENDINGS
 from .states import States
+from .location import get_location
